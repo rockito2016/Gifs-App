734f6bc283b1a44a534015e0ad573b8e14e884a2
Javier Alejandro Pascuaza Cordoba <javieral.pascuaza@umariana.edu.co>
2024-09-02 17:53:19 -0500

    UpdatedServiceTag

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -5,6 +5,8 @@ export class GifsService {
 
   private _tagHistory: string[] = []
 
+  private apiKey: string = 'rfAtMGfhN6ZwaxWdPssGdHGvx3XdTkSs';
+
   constructor() { }
 
   /* Utilizamos el operador spred para crear una copia de valor de los tag history
@@ -14,11 +16,21 @@ export class GifsService {
     return [...this._tagHistory];
   }
 
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag)
+    }
+
+    this._tagHistory.unshift(tag);
+    this._tagHistory = this.tagsHistory.splice(0, 10);
+  }
+
   public searchTag(tag: string): void {
 
     if (tag.length == 0) return;
-
-    this._tagHistory.unshift(tag);
+    this.organizeHistory(tag);
 
     console.log(this.tagsHistory);
 
