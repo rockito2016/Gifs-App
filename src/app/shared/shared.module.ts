5bd0a3eb4be9697f9fb4999ce71109b3b07f56dd
Javier Alejandro Pascuaza Cordoba <javieral.pascuaza@umariana.edu.co>
2024-09-06 15:54:52 -0500

    componentsMoreSmall

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    SidebarComponent
+    SidebarComponent,
+    LazyImageComponent
   ]
 })
 export class SharedModule { }
