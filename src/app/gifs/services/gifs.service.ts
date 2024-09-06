6973533b7c455ace2b8342a9e1c215a7f0cc0100
Javier Alejandro Pascuaza Cordoba <javieral.pascuaza@umariana.edu.co>
2024-09-05 21:25:49 -0500

    ShowGifsInKeepTheTag

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,10 @@ export class GifsService {
   private apiKey: string = 'rfAtMGfhN6ZwaxWdPssGdHGvx3XdTkSs';
   private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs has been received');
+  }
 
   /* Utilizamos el operador spred para crear una copia de valor de los tag history
   Esto se da ya que en JS los array pasan por referencia */
@@ -29,11 +32,22 @@ export class GifsService {
 
     this._tagHistory.unshift(tag);
     this._tagHistory = this.tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
   }
 
 
   private saveLocalStorage(): void {
     localStorage.setItem('history', JSON.stringify(this._tagHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+    /* Aquí obtenemos el stringify del objeto de strings que almacena a nuestros GIFS.
+    Esto lo hacemos por medio de JSON.parse y el operator not null para indicar que siempre recibiremos data */
+    this._tagHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if (this._tagHistory.length === 0) return;
+    this.searchTag(this._tagHistory[0]);
   }
 
   public searchTag(tag: string): void {
