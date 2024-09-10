64a0a327e78c3325d9fa9975400257d1e9a28188
Javier Alejandro Pascuaza Cordoba <javieral.pascuaza@umariana.edu.co>
2024-09-09 19:08:15 -0500

    SaveChanges

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -19,8 +19,10 @@ export class LazyImageComponent implements OnInit {
   }
 
   onLoad() {
-    console.log('Image loaded');
-    this.hasLoaded = true;
+    setTimeout(() => {
+      this.hasLoaded = true;
+    }, 1000);
+
   }
 
 }
